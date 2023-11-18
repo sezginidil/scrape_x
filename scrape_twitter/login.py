56f8387bcd8c625a39bd2fbbc49f820559e9dd48
sezginidil <idil.sezgin@tum.de>
2023-11-18 15:04:07 +0100

    first commit

---
+++ scrape_twitter/login.py
@@ -0,0 +1,14 @@
+"""Information about login.py file functions 
+
+"""
+
+def login(username: str, password: str) -> bool:
+    """
+    Args:
+        username (str): username for login.
+        password (str): corresponding password for login.
+
+    Returns:
+        bool: True, if login successes; otherwise False.
+    """
+    return True
